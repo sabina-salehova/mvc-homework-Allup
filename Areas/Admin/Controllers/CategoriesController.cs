e81b647dac64e8593ed5fd9700ca99520893d650
sabina-salehova <sabinaas@code.edu.az>
2023-01-03 06:58:29 -0800

    added update category

--- Areas/Admin/Controllers/CategoriesController.cs
+++ Areas/Admin/Controllers/CategoriesController.cs
@@ -29,7 +29,7 @@ namespace Allup.Areas.Admin.Controllers
             var categories = await _dbContext.Categories.Where(c => !c.IsDeleted && c.IsMain).ToListAsync();
             var categoryListItems = new List<SelectListItem>();
 
-            categoryListItems.Add(new SelectListItem(" --- Select List Item --- ","0"));
+            categoryListItems.Add(new SelectListItem(" --- Select List Item --- ", "0"));
 
             categories.ForEach(c => categoryListItems.Add(new SelectListItem(c.Name, c.Id.ToString())));
 
@@ -49,6 +49,7 @@ namespace Allup.Areas.Admin.Controllers
                 .Where(c => !c.IsDeleted && c.IsMain)
                 .Include(x => x.Children)
                 .ToListAsync();
+
             var categoryListItems = new List<SelectListItem>();
 
             categoryListItems.Add(new SelectListItem(" --- Select List Item --- ", "0"));
@@ -71,7 +72,7 @@ namespace Allup.Areas.Admin.Controllers
             {
                 if (parentCategories.Any(c => c.Name.ToLower().Equals(model.Name.ToLower())))
                 {
-                    ModelState.AddModelError("", "Bu adli category movcuddur");
+                    ModelState.AddModelError("", "Bu adli parent category movcuddur");
                     return View(viewModel);
                 }
 
@@ -91,7 +92,7 @@ namespace Allup.Areas.Admin.Controllers
 
                 createCategory.ImageUrl = unicalName;
             }
-            else 
+            else
             {
                 if (model.ParentId == 0)
                 {
@@ -120,5 +121,167 @@ namespace Allup.Areas.Admin.Controllers
 
             return RedirectToAction(nameof(Index));
         }
+
+        public async Task<IActionResult> Update(int? id)
+        {
+            if (id is null) return NotFound();
+
+            var existCategory = await _dbContext.Categories.Where(c => !c.IsDeleted && c.Id == id).FirstOrDefaultAsync();
+
+            if (existCategory is null) return NotFound();
+
+            var updateCategory = new CategoryUpdateViewModel
+            {
+                Id = existCategory.Id,
+                Name = existCategory.Name,
+                ParentId = existCategory.ParentId,
+                IsMain = existCategory.IsMain,
+                ImageUrl = existCategory.ImageUrl
+            };
+
+            var categories = await _dbContext.Categories.Where(c => !c.IsDeleted && c.IsMain).ToListAsync();
+            var categoriesSelectListItem = new List<SelectListItem>();
+            categories.ForEach(x => categoriesSelectListItem.Add(new SelectListItem(x.Name, x.Id.ToString())));
+            updateCategory.ParentCategories = categoriesSelectListItem;
+
+            return View(updateCategory);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Update(CategoryUpdateViewModel model)
+        {
+            if (model.Id is null)
+                return NotFound();
+
+            var existCategory = await _dbContext.Categories.FindAsync(model.Id);
+
+            if (existCategory is null)
+                return NotFound();
+
+            if (!ModelState.IsValid)
+            {
+                return View(model);
+            }            
+
+            var updateImage = existCategory.ImageUrl;
+
+            var parentCategories = await _dbContext.Categories
+               .Where(c => !c.IsDeleted && c.IsMain)
+               .Include(x => x.Children)
+               .ToListAsync();
+
+            if (model.IsMain)
+            {
+                if (parentCategories.Any(c => c.Id!=model.Id && c.Name.ToLower().Equals(model.Name.ToLower())))
+                {
+                    ModelState.AddModelError("", "Bu adli parent category movcuddur");
+                    return View(model);
+                }
+
+                if (updateImage is null)
+                {
+                    if (!model.Image.IsImage())
+                    {
+                        ModelState.AddModelError("", "Sekil secmelisiz");
+                        return View(model);
+                    }
+                }
+
+                existCategory.ParentId = null;
+            }
+            else
+            {
+                if (existCategory.ParentId==0)
+                {
+                    if (model.ParentId == 0)
+                    {
+                        ModelState.AddModelError("", "Parent category secilmelidir");
+                        return View(model);
+                    }                    
+                }
+
+                if (model.ParentId != 0)
+                {
+                    var childCategories = await _dbContext.Categories
+                    .Where(c => !c.IsDeleted && !c.IsMain && c.ParentId==model.ParentId && c.Id!=existCategory.Id)
+                    .ToListAsync();
+
+                    if (childCategories.Any(c => c.Name.ToLower().Equals(model.Name.ToLower())))
+                    {
+                        ModelState.AddModelError("", "Bu adli alt category movcuddur");
+                        return View(model);
+                    }
+
+                    existCategory.ParentId = model.ParentId;
+                }
+
+                existCategory.ImageUrl = "";
+            }
+
+            if (model.Image is not null)
+            {
+                var newImage = model.Image;
+
+                if (!newImage.IsImage())
+                {
+                    ModelState.AddModelError("Image", "Shekil formati secilmelidir");
+
+                    return View(model);
+                }
+                int imageMbCount = 10;
+                if (!newImage.IsAllowedSize(imageMbCount))
+                {
+                    ModelState.AddModelError("Image", $"Shekilin olcusu {imageMbCount} mb-dan boyuk ola bilmez");
+
+                    return View(model);
+                }
+
+                var path = Path.Combine(Constants.CategoryPath, existCategory.ImageUrl);
+
+                if (System.IO.File.Exists(path))
+                    System.IO.File.Delete(path);
+
+                var unicalFileName = await model.Image.GenerateFile(Constants.CategoryPath);
+
+                updateImage = unicalFileName;
+                existCategory.ImageUrl = updateImage;
+            }
+
+            existCategory.Name = model.Name;
+            existCategory.IsMain = model.IsMain;
+
+            await _dbContext.SaveChangesAsync();
+
+            return RedirectToAction(nameof(Index));
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id is null) return NotFound();
+
+            var existCategory = await _dbContext.Categories.FindAsync(id);
+
+            if (existCategory is null) return NotFound();
+
+            if (existCategory.IsMain)
+            {
+                var children = await _dbContext.Categories.Where(c => !c.IsMain && c.ParentId == id).ToListAsync();
+                children.ForEach(c => _dbContext.Categories.Remove(c));
+            }
+
+            var path = Path.Combine(Constants.CategoryPath, existCategory.ImageUrl);
+
+            if (System.IO.File.Exists(path))
+                System.IO.File.Delete(path);
+
+            _dbContext.Categories.Remove(existCategory);
+
+            await _dbContext.SaveChangesAsync();
+
+            return RedirectToAction(nameof(Index));
+        }
+
     }
 }
---
+++ Areas/Admin/Models/CategoryUpdateViewModel.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace Allup.Areas.Admin.Models
+{
+    public class CategoryUpdateViewModel
+    {
+        public int? Id { get; set; }
+        public string Name { get; set; }
+        public bool IsMain { get; set; }
+        public IFormFile? Image { get; set; }
+        public string ImageUrl { get; set; } = String.Empty;
+        public int? ParentId { get; set; }
+        public List<SelectListItem> ParentCategories { get; set; } = new();
+    }
+}
